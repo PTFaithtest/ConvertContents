ad98cdc581a91f21e01fd4441a711f63ca6c1157
PTFaithtest <61067443+PTFaithtest@users.noreply.github.com>
2023-03-31 15:24:40 -0700

    Handle all language tags, Disallow column headings , locate all documents in ConvertContents folder

--- bundle.py
+++ bundle.py
@@ -1,20 +1,39 @@
+# TO DO
+# fix quoteless use this product to test so that it gets rid of every instance: https://librarymanager.faithlife.com/metadata/%7b13CFFC11-9C19-450C-82FE-2E61378531A3%7d
+# code for divider has been modified but not tested, can be compared with code on github
+# replace only works on first instance, so better way needs to be used
+# code needs to be worked out for 'nuther" which was previously overlooked
+# consolidate code so it is not makeing so many lists that get replaced by subsequent lists
+# get rid of blank lines in the beginning
+# perhaps disallow marketing tags like automatic updates
+
+
 import os
-with open('raw_bundle.txt', 'r+', encoding='utf-8') as bundle:
+from pathlib import Path
+
+ignore = ['Top Title (Value ~ Language)', 'Titles (Value ~ Language)']
+
+raw = Path.home().joinpath('ConvertContents', 'raw_bundle.txt')
+with open(raw, 'r+', encoding='utf-8') as bundle:
     better = []
     cleaner = []
+    quoteless = []
     dashless = []
     new_bundle = []
     artless = []
     articles = ['A ', 'An ', 'The ']
     divider = '|'
-    suffix = '~en'
     messy = 'â€™'
     dash = 'â€“'
+    quote = 'â€œ'
+    nuther = 'â€'
     for line in bundle:
-        # print(f'{type(line)}: {line}')
         old_string = line.strip()
-        if suffix in old_string:
-            better_string = old_string.replace(suffix, '', 5)
+        if old_string in ignore:
+            continue
+        elif len(old_string) > 2 and old_string[-3] == '~':
+            truncated = slice(0, -3)
+            better_string = old_string[truncated]
         else: 
             better_string = old_string
         better.append(better_string)
@@ -32,30 +51,32 @@ with open('raw_bundle.txt', 'r+', encoding='utf-8') as bundle:
             artless.append(bet)   
     for rtl in artless:
         if messy in rtl:
-            clean_string = rtl.replace(messy, "'", 5)
+            clean_string = rtl.replace(messy, "'")
         else:
             clean_string = rtl
         cleaner.append(clean_string)
     for cl in cleaner:
         if dash in cl:
-            dashless_string = cl.replace(dash, '-', 5)      
+            dashless_string = cl.replace(dash, '-')      
         else:
             dashless_string = cl
         dashless.append(dashless_string)
-    for di in dashless:
-        if divider in di:
-            for i in range(len(di)):
-                if di[i] != divider:
-                    continue
-                else:
-                    new_string = di[i+1:]
-                    break                   
+    for da in dashless:
+        if quote in da:
+            quote_string = da.replace(quote, '')
         else:
-            new_string = di
+            quote_string = da
+        quoteless.append(quote_string)   
+    for qu in quoteless:      
+        if divider in qu:
+            new_string = qu.translate(divider, '')              
+        else:
+            new_string = qu
         new_bundle.append(new_string)
     new_bundle.sort()
-with open('BundleContents.txt', 'w+', encoding='utf-8') as new_file:
+contents = Path.home().joinpath('ConvertContents', 'BundleContents.txt')    
+with open(contents, 'w+', encoding='utf-8') as new_file:
     for i in new_bundle:
         new_file.write(f'{i}\n')
-os.startfile('BundleContents.txt')
+os.startfile(contents)
 
