fab54dc9c18bf618b1be9b89368028b3da2f1882
PTFaithtest <61067443+PTFaithtest@users.noreply.github.com>
2023-04-14 12:38:49 -0700

    Filter out more bad characters, eliminate excessive lists

--- bundle.py
+++ bundle.py
@@ -1,82 +1,59 @@
-# TO DO
-# fix quoteless use this product to test so that it gets rid of every instance: https://librarymanager.faithlife.com/metadata/%7b13CFFC11-9C19-450C-82FE-2E61378531A3%7d
-# code for divider has been modified but not tested, can be compared with code on github
-# replace only works on first instance, so better way needs to be used
-# code needs to be worked out for 'nuther" which was previously overlooked
-# consolidate code so it is not makeing so many lists that get replaced by subsequent lists
-# get rid of blank lines in the beginning
-# perhaps disallow marketing tags like automatic updates
-
-
 import os
 from pathlib import Path
 
 ignore = ['Top Title (Value ~ Language)', 'Titles (Value ~ Language)']
-
 raw = Path.home().joinpath('ConvertContents', 'raw_bundle.txt')
+
+def replace_chars(title, sub_string, new_char):
+    if sub_string in title:
+        new_string = title.replace(sub_string, new_char)
+        return new_string
+    else:
+        return title
+
 with open(raw, 'r+', encoding='utf-8') as bundle:
     better = []
-    cleaner = []
-    quoteless = []
-    dashless = []
-    new_bundle = []
-    artless = []
     articles = ['A ', 'An ', 'The ']
-    divider = '|'
     messy = 'â€™'
     dash = 'â€“'
     quote = 'â€œ'
-    nuther = 'â€'
+    nuther = 'â€\x9d'
+    divider = '| '
+
+
     for line in bundle:
         old_string = line.strip()
         if old_string in ignore:
             continue
-        elif len(old_string) > 2 and old_string[-3] == '~':
+        if len(old_string) > 2 and old_string[-3] == '~':
             truncated = slice(0, -3)
             better_string = old_string[truncated]
-        else: 
+        else:
             better_string = old_string
-        better.append(better_string)
-    for bet in better:
-        untouched = True
+        if better_string.startswith('Automatic') and better_string.endswith('Update') or better_string.endswith('Marker Resource'):
+            continue
+        else:
+            real_title = better_string
+        clean_string = replace_chars(real_title, messy, '\'')
+        dashless_string = replace_chars(clean_string, dash, '-')
+        quote_string = replace_chars(dashless_string, quote, '')
+        nuther_string = replace_chars(quote_string, nuther, '')
+        undivided_string = replace_chars(nuther_string, divider, '')
         for art in articles:
-            if bet.startswith(art):
-                artless_string = bet.replace(art, '')
+            untouched = True
+            if undivided_string.startswith(art):
+                artless_string = undivided_string.replace(art, '')
                 untouched = False
-                artless.append(artless_string)
                 break
             else:
                 continue
         if untouched:
-            artless.append(bet)   
-    for rtl in artless:
-        if messy in rtl:
-            clean_string = rtl.replace(messy, "'")
-        else:
-            clean_string = rtl
-        cleaner.append(clean_string)
-    for cl in cleaner:
-        if dash in cl:
-            dashless_string = cl.replace(dash, '-')      
-        else:
-            dashless_string = cl
-        dashless.append(dashless_string)
-    for da in dashless:
-        if quote in da:
-            quote_string = da.replace(quote, '')
-        else:
-            quote_string = da
-        quoteless.append(quote_string)   
-    for qu in quoteless:      
-        if divider in qu:
-            new_string = qu.translate(divider, '')              
-        else:
-            new_string = qu
-        new_bundle.append(new_string)
-    new_bundle.sort()
-contents = Path.home().joinpath('ConvertContents', 'BundleContents.txt')    
+            artless_string = undivided_string
+        if len(artless_string) > 0:
+            better.append(artless_string)       
+    better.sort()
+contents = Path.home().joinpath('ConvertContents', 'BundleContents.txt')
 with open(contents, 'w+', encoding='utf-8') as new_file:
-    for i in new_bundle:
+    for i in better:
         new_file.write(f'{i}\n')
-os.startfile(contents)
-
+os.startfile(contents)   
