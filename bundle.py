3f27ef8bafc71399dc177d677242d3b3657b7ba6
PTFaithtest <61067443+PTFaithtest@users.noreply.github.com>
2023-04-26 15:19:21 -0700

    ReFactor code, Add German, French

--- bundle.py
+++ bundle.py
@@ -11,13 +11,6 @@ def remove_articles(current_line, art_fam):
         else:
             continue
     return current_line
-
-def replace_chars(title, sub_string, new_char):
-    if sub_string in title:
-        new_string = title.replace(sub_string, new_char)
-        return new_string
-    else:
-        return title
     
 def convert_process(current_line, convert_dict):
     print(f'before: {current_line}')
@@ -30,10 +23,13 @@ def convert_process(current_line, convert_dict):
 
 with open(raw, 'r+', encoding='utf-8') as bundle:
     better = []
-    articles = ['A ', 'An ', 'The ', 'Un ', 'Una ', 'Unos ', 'Unas ', 'El ', 'Los ', 'La ', 'Las ', 'Lo ', 'Der ', 'Die ', 'Das ', 'Ein ', 'Eine ']
+    articles = [
+        'A ', 'An ', 'Der ', 'Die ', 'Das ', 'Ein ', 'Eine ', 'El ', 'L\'', 'La ', 'Las ', 'Le ', 'Les ', 'Lo ',  'Los ', 'The ', 'Un ', 'Una ', 'Unas ', 'Une ', 'Unos ' 
+        ]
     char_dict = {
-        'â€™': '\'', 'â€“': '-', 'â€œ': '', 'â€\x9d': '', '| ': '', 'Ã³': 'ó', 'Ã±': 'ñ', 'â€¦': '…', 'Ã­': 'í', 'Ã“': 'Ó', 'Ã©': 'é', 'Ã¡': 'á', 'Â¿': '¿', 'â€”': '—', 'Ã¼': 'ü',
-        'ãº': 'ú', 'Ã‰': 'É', 'Â¡': '¡'
+        'â€™': '\'', 'â€“': '-', 'â€œ': '“', 'â€\x9d': '”', '| ': '', 'Ã³': 'ó', 'Ã±': 'ñ', 'â€¦': '…', 'Ã­': 'í', 'Ã“': 'Ó', 'Ã©': 'é', 'Ã¡': 'á', 'Â¿': '¿', 'â€”': '—', 'Ã¼': 'ü',
+        'ãº': 'ú', 'Ã‰': 'É', 'Â¡': '¡', 'Ã¶': 'ö', 'Ã¤': 'ä', 'â€ž': '„', 'Â»': '»', 'â«': '«', 'Ã„': 'Ä', 'Ãœ': 'Ü', 'Ã–': 'Ö', 'uÌˆ': 'ü','Å™': 'ř', 'Ã§': 'ç', 'Ãª': 'ê', 'Ã¨': 'è',
+        'Ã': 'à'
         }
     for line in bundle:
         old_string = line.strip()
@@ -45,22 +41,33 @@ with open(raw, 'r+', encoding='utf-8') as bundle:
         if better_string.startswith('Automatic') and better_string.endswith('Update') or better_string.endswith('Marker Resource'): 
             continue
         elif better_string.startswith('Mobile Ed Course (You Choose)') or better_string.endswith('Mobile Ed Bonus Offer'):
-            continue
+            continue       
         else:
             real_title = better_string
-        if real_title.startswith('Â¿'):
-            real_title = real_title.replace('Â¿', '')
-            real_title = real_title.replace('?', '')
-        elif real_title.startswith('Â¡'):
-            real_title = real_title.replace('Â¡', '')
-            real_title = real_title.replace('!', '')
         artless_string = remove_articles(real_title, articles)
+        if artless_string.startswith('â€œ'):
+            artless_string = artless_string.replace('â€œ', '', 1)
+            artless_string = artless_string.replace('â€\x9d', '', 1)
+        elif artless_string.startswith('Â¿'):
+            artless_string = artless_string.replace('Â¿', '', 1)
+            artless_string = artless_string.replace('?', '', 1)
+        elif artless_string.startswith('Â¡'):
+            artless_string = artless_string.replace('Â¡', '', 1)
+            artless_string = artless_string.replace('!', '', 1)
+        elif artless_string.startswith('('):
+            artless_string = artless_string.replace('(', '', 1)
+            artless_string = artless_string.replace(')', '', 1)
+        elif artless_string.startswith('Ãœ'):
+            artless_string = artless_string.replace('Ãœ', 'U', 1)
+        elif artless_string.startswith('Ã'):
+            artless_string = artless_string.replace('Ã', 'A', 1)
+        elif artless_string.startswith('Ö'):
+            artless_string = artless_string.replace('Ö', 'O', 1)
+        elif 'Ð¡Ð¸Ð½Ð¾Ð´Ð°Ð»ÑŒÐ½Ñ‹Ð¹ ÐŸÐµÑ€ÐµÐ²Ð¾Ð´ (1876/1956)' in artless_string:
+            artless_string = 'Русский Синодальный Перевод (1876/1956)'
         converted_string = convert_process(artless_string, char_dict)      
-        capitalized_string = converted_string.title()
-        if '\'S' in capitalized_string:
-            capitalized_string = capitalized_string.replace('\'S', '\'s')
-        if len(capitalized_string) > 0:
-            better.append(capitalized_string)       
+        capitalized_string = converted_string[0].upper() + converted_string[1:]   
+        better.append(capitalized_string)       
     better.sort()
 contents = Path.home().joinpath('ConvertContents', 'BundleContents.txt')
 with open(contents, 'w+', encoding='utf-8') as new_file:
